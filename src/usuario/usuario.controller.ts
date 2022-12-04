4b19957fbcbe28a04a37bd4e0cd1e50702f1fabb
pequenojoohn <jonathan.matos2445@gmail.com>
2022-12-04 03:04:34 -0300

    Crud for users create

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
+import { UsuarioEntity } from './usuario.entity';
 import { UsuarioRepository } from './usuario.repository';
+import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from './dto/ListaUsuario.dto';
+import { AtualizaUsuarioDTO } from './dto/AtualizaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -8,12 +12,50 @@ export class UsuarioController {
 
   @Post()
   async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-    this.usuarioRepository.salvar(dadosDoUsuario);
-    return dadosDoUsuario;
+    const usuarioEntity = new UsuarioEntity();
+    usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.id = uuid();
+
+    this.usuarioRepository.salvar(usuarioEntity);
+
+    return {
+      usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+      message: 'Usuário criado com sucesso',
+    };
   }
 
   @Get()
   async listarUsuarios() {
-    return this.usuarioRepository.listar();
+    const usuariosSalvos = await this.usuarioRepository.listar();
+    const usuarisosLista = usuariosSalvos.map(
+      usuario => new ListaUsuarioDTO(
+        usuario.id,
+        usuario.nome
+      )
+    )
+
+    return usuarisosLista;
+  }
+
+  @Put('/:id')
+  async atualizaUsuario(@Param('id') id: string, @Body() novosDados: AtualizaUsuarioDTO) {
+    const usuarioAtualizado = await this.usuarioRepository.atualiza(id, novosDados);
+
+    return {
+      usuario: usuarioAtualizado,
+      message: 'Usuário atualizado com sucesso'
+    }
+  }
+
+  @Delete('/:id')
+  async removeUsuario(@Param('id') id: string) {
+    const usuarioRemovido = await this.usuarioRepository.remove(id);
+
+    return {
+      usuario: usuarioRemovido,
+      message: "Usuário removido com sucesso"
+    }
   }
 }
--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ListaUsuarioDTO } from './dto/ListaUsuario.dto';
+import { UsuarioEntity } from './usuario.entity';
 
 @Injectable()
 export class UsuarioRepository {
-  private usuarios = [];
+  private usuarios: UsuarioEntity[] = [];
 
-  async salvar(usuario) {
+  private buscaPorId(id: string) {
+    const possivelUsuario = this.usuarios.find(
+      usuarioSalvo => usuarioSalvo.id === id
+    );
+
+    if (!possivelUsuario) {
+      throw new Error("Usuário não existe");
+    }
+
+    return possivelUsuario;
+  }
+
+  async salvar(usuario: UsuarioEntity) {
     this.usuarios.push(usuario);
   }
 
@@ -18,5 +32,32 @@ export class UsuarioRepository {
     );
 
     return possivelUsuario !== undefined;
+  }
+
+  async atualiza(id: string, dadosDeAtualizacao: Partial<UsuarioEntity>) {
+    const usuario = this.buscaPorId(id)
+
+
+
+    Object.entries(dadosDeAtualizacao).forEach(([chave, valor]) => {
+      if (chave === 'id') {
+        return;
+      }
+
+      usuario[chave] = valor;
+    });
+
+    return {
+      usuario: new ListaUsuarioDTO(id, dadosDeAtualizacao.nome)
+    };
+  }
+
+  async remove(id: string) {
+    const usuario = this.buscaPorId(id);
+
+    this.usuarios = this.usuarios.filter((usuarioSalvo) => usuarioSalvo.id !== id)
+
+    return usuario;
+
   }
 }
